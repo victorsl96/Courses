ae8acd9d300b3b62ea509ff808a3782b6c9992da
Victor Souza Lopes <victorsl96@gmail.com>
2021-12-07 13:08:28 -0300

    Added a function to store the data into a csv file and modified the get_same_or_newer function to read from the csv file

--- Coursera/debugging.py
+++ Coursera/debugging.py
@@ -30,44 +30,67 @@ def get_file_lines(url):
     # Download the file over the internet
     response = requests.get(url, stream=True)
     lines = []
-
     for line in response.iter_lines():
         lines.append(line.decode("UTF-8"))
     return lines
 
 
+def store_data():
+    '''Stores the data for further use as a csv file'''
+    data = get_file_lines(FILE_URL)
+    with open('data.csv', 'w', newline='') as data_file:
+        writer = csv.writer(data_file)
+        for line in data:
+            writer.writerow(line.split(','))
+
+
 def get_same_or_newer(start_date):
     """Returns the employees that started on the given date, or the closest one."""
-    data = get_file_lines(FILE_URL)
-    reader = csv.reader(data[1:])
+    store_data()
+    data = []
+    with open('data.csv', 'r') as data_file:
+        reader = csv.reader(data_file)
+        for row in reader:
+            # if row[0]:
+            #     continue
+            data.append(row)
+    # data = get_file_lines(FILE_URL)
+    # reader = csv.reader(data[1:])
 
     # We want all employees that started at the same date or the closest newer
     # date. To calculate that, we go through all the data and find the
     # employees that started on the smallest date that's equal or bigger than
     # the given start date.
+
     min_date = datetime.datetime.today()
     min_date_employees = []
-    for row in reader:
-        row_date = datetime.datetime.strptime(row[3], '%Y-%m-%d')
+    # data_dict = {}
+    for item in data[1:]:
+        row_date = datetime.datetime.strptime(item[3], '%Y-%m-%d')
+        # data_dict[row_date]
 
-        # If this date is smaller than the one we're looking for,
-        # we skip this row
+      # If this date is smaller than the one we're looking for,
+      # we skip this row
+
+    #   ------
         if row_date < start_date:
             continue
-
+    #   ------
         # If this date is smaller than the current minimum,
         # we pick it as the new minimum, resetting the list of
         # employees at the minimal date.
+    #   ------
         if row_date < min_date:
             min_date = row_date
             min_date_employees = []
-
+    #   ------
         # If this date is the same as the current minimum,
         # we add the employee in this row to the list of
         # employees at the minimal date.
+    #   ------
         if row_date == min_date:
-            min_date_employees.append("{} {}".format(row[0], row[1]))
-
+            min_date_employees.append("{} {}".format(item[0], item[1]))
+    #   ------
     return min_date, min_date_employees
 
 
